107b333222ec9ecbbda67d3f83546eb3bc34b61e
youcix <youcix3403@gmail.com>
2020-11-17 20:51:10 +0800

    new

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ new Vue ({
     el:"#content",
     data:{
         datas:null
+    },
+
+
+// 所有元素顯示後執行一次
+mounted(){
+    // 新增 XML 物件
+    var xhr = new XMLHttpRequest();
+    // 開啟並指定 API 網址
+    xhr.open("GET","./datas.JSON");
+    // 傳送要求
+    xhr.send();
+    // 載入完成 = () => {} - 匿名函式 = function name () {}
+    xhr.onload = () => {
+        // 將 JSON 資料轉為物件
+        var obj = JSON.parse(xhr.responseText);
+        // 資料 = 物件.資料名稱.資料名稱
+        this.datas = obj.result.records;
+
+        console.log(this.datas);
+        }
     }
 
 });
-
-mounted(){
-    var xhr = new xhr XMLHttpRequest();
-    xhr.open("get","https://data.tycg.gov.tw/api/v1/rest/datastore/a7db5f95-ac1c-4597-8bf5-c5be0e57ae5f?format=json");
-    xhr.send();
-}
